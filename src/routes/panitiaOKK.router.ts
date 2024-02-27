872724232e79ad151e60d7de96124465cb376f4e
Adrian <adrian.hamzah15@gmail.com>
2024-02-28 00:40:37 +0700

    Added isiAbsensi path for rapat

--- src/routes/panitiaOKK.router.ts
+++ src/routes/panitiaOKK.router.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { getAllRapatSession, makeRapatSession } from '../controllers/panitiaOKK.controller'
+import { getAllRapatSession, isiAbsensiRapat, makeRapatSession } from '../controllers/panitiaOKK.controller'
 import { authenticatePanitia, authenticateUser } from '../middlewares/auth.middleware'
 
 const panitiaOKKRouter: Router = Router()
@@ -7,6 +7,7 @@ const panitiaOKKRouter: Router = Router()
 panitiaOKKRouter.use(authenticateUser)
 panitiaOKKRouter.use(authenticatePanitia)
 
+panitiaOKKRouter.patch('/isi-absensi-rapat', isiAbsensiRapat)
 panitiaOKKRouter.get('/get-all-rapat', getAllRapatSession)
 panitiaOKKRouter.post('/create-rapat', makeRapatSession)
 
