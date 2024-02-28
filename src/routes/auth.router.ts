7e601b85c824234db83ed8b96b866f8bdd28ffaf
Adrian <adrian.hamzah15@gmail.com>
2024-02-28 16:38:40 +0700

    adding a delete all database DELETE req

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
-import { register, login, deleteAll, deleteAllToken, getAllMentor, getAllPeserta, getAllPanitia, deleteAllMeeting } from '../controllers/auth.controller'
+import { register, login, deleteAll, deleteAllToken, getAllMentor, getAllPeserta, getAllPanitia, deleteAllMeeting, getAllSponsor, getAllPembicara, deleteAllSponsor_Pembicara_Acara_Proposal } from '../controllers/auth.controller'
 
 const authRouter: Router = Router()
 
 authRouter.get('/get-all-peserta', getAllPeserta)
 authRouter.get('/get-all-mentor', getAllMentor)
 authRouter.get('/get-all-panitia', getAllPanitia)
+authRouter.get('/get-all-sponsor', getAllSponsor)
+authRouter.get('/get-all-pembicara', getAllPembicara)
 
 authRouter.post('/register', register)
 authRouter.post('/login', login)
@@ -13,5 +15,6 @@ authRouter.post('/login', login)
 authRouter.delete('/delete-all', deleteAll)
 authRouter.delete('/delete-all-token', deleteAllToken)
 authRouter.delete('/delete-all-meeting', deleteAllMeeting)
+authRouter.delete('/delete-all-sponsor-pembicara-acara-proposal', deleteAllSponsor_Pembicara_Acara_Proposal)
 
 export default authRouter
